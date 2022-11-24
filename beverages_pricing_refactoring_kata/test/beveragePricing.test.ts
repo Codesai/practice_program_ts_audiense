aa2b40a7eb41237014e056c46417b0bf482954cf
freyes <fran@codesai.com>
2022-11-24 12:43:25 +0000

    fix toBeClose digits

--- beverages_pricing_refactoring_kata/test/beveragePricing.test.ts
+++ beverages_pricing_refactoring_kata/test/beveragePricing.test.ts
@@ -11,42 +11,42 @@ describe('Beverage pricing', () => {
   it('computes coffee price', () => {
     const coffee = new Coffee();
 
-    expect(coffee.price()) .toBeCloseTo(1.20, 0.001);
+    expect(coffee.price()) .toBeCloseTo(1.20, 2);
   });
 
   it('computes tea price', () => {
     const tea = new Tea();
 
-    expect(tea.price()) .toBeCloseTo(1.50, 0.001);
+    expect(tea.price()) .toBeCloseTo(1.50, 2);
   });
 
   it('computes hot chocolate price', () => {
     const hotChocolate = new HotChocolate();
 
-    expect(hotChocolate.price()) .toBeCloseTo(1.45, 0.001);
+    expect(hotChocolate.price()) .toBeCloseTo(1.45, 2);
   });
 
   it('computes tea with milk price', () => {
     const teaWithMilk = new TeaWithMilk();
 
-    expect(teaWithMilk.price()) .toBeCloseTo(1.60, 0.001);
+    expect(teaWithMilk.price()) .toBeCloseTo(1.60, 2);
   });
 
   it('computes coffee with milk price', () => {
     const coffeeWithMilk = new CoffeeWithMilk();
 
-    expect(coffeeWithMilk.price()) .toBeCloseTo(1.30, 0.001);
+    expect(coffeeWithMilk.price()) .toBeCloseTo(1.30, 2);
   });
 
   it('computes coffee with milk and cream price', () => {
     const coffeeWithMilkAndCream = new CoffeeWithMilkAndCream();
 
-    expect(coffeeWithMilkAndCream.price()) .toBeCloseTo(1.45, 0.001);
+    expect(coffeeWithMilkAndCream.price()) .toBeCloseTo(1.45, 2);
   });
 
   it('computes hot chocolate with cream price', () => {
     const hotChocolateWithCream = new HotChocolateWithCream();
 
-    expect(hotChocolateWithCream.price()).toBeCloseTo(1.60, 0.001);
+    expect(hotChocolateWithCream.price()).toBeCloseTo(1.60, 2);
   });
 });
