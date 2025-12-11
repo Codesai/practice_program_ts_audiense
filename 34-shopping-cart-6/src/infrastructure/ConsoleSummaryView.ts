701af1989afff5238c267a546ce05b832ba18e1f
freyes <fran@codesai.com>
2025-12-11 09:44:22 +0000

    undo fix error

--- 34-shopping-cart-6/src/infrastructure/ConsoleSummaryView.ts
+++ 34-shopping-cart-6/src/infrastructure/ConsoleSummaryView.ts
@@ -42,7 +42,7 @@ export class ConsoleSummaryView implements SummaryView {
     }
 
     private static ordersText(content: CartSummary): string {
-        return content.orders.reduce<string>(
+        return content.orders.reduce(
             (acc, order) => {
                 return acc + this.formatOrder(order) + this.LINE_BREAK;
             },
