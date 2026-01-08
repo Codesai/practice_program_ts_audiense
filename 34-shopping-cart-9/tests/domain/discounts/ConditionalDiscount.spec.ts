3890c33cb7d00e3cee701559f859a959844f391e
trikitrok <trikitrok@gmail.com>
2026-01-08 20:14:50 +0100

    removing duplication and adding link to an alternative version

--- 34-shopping-cart-9/tests/domain/discounts/ConditionalDiscount.spec.ts
+++ 34-shopping-cart-9/tests/domain/discounts/ConditionalDiscount.spec.ts
@@ -6,47 +6,44 @@ import {
 import {aFixedDiscountDTO} from "../../helpers/DiscountDtoBuilder";
 
 describe('DependingOnMinimumAmountDiscount', () => {
-    const amountAfterApplyingDiscount = 10;
     const minimumRequiredAmount = 100;
-
     let decoratedDiscount: jest.Mocked<Discount>;
+    let conditionalDiscount: Discount;
 
     beforeEach(() => {
         decoratedDiscount = {
             applyTo: jest.fn(),
             toDto: jest.fn()
         };
+        conditionalDiscount = aDiscountAppliedWhenOverMinimumAmount(decoratedDiscount, minimumRequiredAmount);
     });
 
     it.each([
         [minimumRequiredAmount],
         [100.01],
         [150]
-    ])('should apply discount when amount (%i) is greater than or equal to minimum amount', (amount: any) => {
-        const conditionalDiscount = aDiscountAppliedWhenOverMinimumAmount(decoratedDiscount, minimumRequiredAmount);
-        when(decoratedDiscount.applyTo).calledWith(amount).mockReturnValue(amountAfterApplyingDiscount);
+    ])('should apply discount when amount (%i) is greater than or equal to the minimum required amount', (amount: number) => {
+        const amountAfterDecoratedDiscount = 10;
+        when(decoratedDiscount.applyTo).calledWith(amount).mockReturnValue(amountAfterDecoratedDiscount);
 
-        const amountAfterDiscount = conditionalDiscount.applyTo(amount);
+        const finalAmount = conditionalDiscount.applyTo(amount);
 
-        expect(amountAfterDiscount).toBe(amountAfterApplyingDiscount);
+        expect(finalAmount).toBe(amountAfterDecoratedDiscount);
     });
 
-    it('should not apply discount when amount is less than minimum amount', () => {
+    it('should not apply discount when amount is less than the minimum required amount', () => {
         const amount = 99.99;
-        const conditionalDiscount = aDiscountAppliedWhenOverMinimumAmount(decoratedDiscount, minimumRequiredAmount);
-        when(decoratedDiscount.applyTo).calledWith(amount).mockReturnValue(amountAfterApplyingDiscount);
 
-        const amountAfterDiscount = conditionalDiscount.applyTo(amount);
+        const finalAmount = conditionalDiscount.applyTo(amount);
 
-        expect(amountAfterDiscount).toBe(amount);
+        expect(finalAmount).toBe(amount);
     });
 
     it('should create its DTO correctly', () => {
-        const conditionalDiscount = aDiscountAppliedWhenOverMinimumAmount(decoratedDiscount, minimumRequiredAmount);
         const decoratedDiscountDto = aFixedDiscountDTO().build();
         when(decoratedDiscount.toDto).mockReturnValue(decoratedDiscountDto);
 
-        let discountDto = conditionalDiscount.toDto();
+        const discountDto = conditionalDiscount.toDto();
 
         expect(discountDto).toStrictEqual(decoratedDiscountDto);
     });
@@ -55,3 +52,5 @@ describe('DependingOnMinimumAmountDiscount', () => {
         return new DependingOnMinimumAmountDiscount(discount, minimumRequiredAmount);
     }
 });
+
+// See an alternative version not using test doubles in https://gist.github.com/trikitrok/f623d5054a6171b0a2a8e13a1f8f438c
