24c1aef7673119f9cfa6d95ff5904930051ac529
trikitrok <trikitrok@gmail.com>
2026-02-19 17:06:58 +0100

    fixing architecture tests

--- 34-shopping-cart-10/tests/architecture/architectureRules.spec.ts
+++ 34-shopping-cart-10/tests/architecture/architectureRules.spec.ts
@@ -1,24 +1,44 @@
-import {filesOfProject} from "tsarch";
+import {DependOnFileCondition, projectFiles} from "archunit";
 
 describe("Architecture Rules", () => {
     it("domain should not depend on infrastructure", async () => {
-        const violations = await filesOfProject()
-            .inFolder("src/domain")
+        const rule = projectFiles()
+            .inFolder('src/domain/**')
             .shouldNot()
             .dependOnFiles()
-            .inFolder("src/infrastructure")
-            .check();
+            .inFolder("src/infrastructure/**");
+        await expect(rule).toPassAsync();
+    });
 
-        expect(violations).toEqual([]);
+    it("sanity check: infrastructure depends on domain (should fail this rule)", async () => {
+        const rule = projectFiles()
+            .inFolder("src/infrastructure/**")
+            .shouldNot()
+            .dependOnFiles()
+            .inFolder("src/domain/**");
+
+        await expectRuleToFail(rule);
     });
 
     it("production code should not have circular dependencies", async () => {
-        const violations = await filesOfProject()
-            .inFolder("src")
+        const rule = projectFiles()
+            .inFolder("src/**")
             .should()
-            .beFreeOfCycles()
-            .check();
+            .haveNoCycles();
 
-        expect(violations).toEqual([]);
+        await expect(rule).toPassAsync();
     });
+
+    it("production code should not depend on tests", async () => {
+        const rule = projectFiles()
+            .inFolder('src/**')
+            .shouldNot()
+            .dependOnFiles()
+            .inFolder("tests/**");
+        await expect(rule).toPassAsync();
+    });
+
+    async function expectRuleToFail(rule: DependOnFileCondition) {
+        await expect(expect(rule).toPassAsync()).rejects.toThrow();
+    }
 });
