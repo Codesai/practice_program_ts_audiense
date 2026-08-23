100caad2f039b7bc311ad8659042790c1634b80e
trikitrok <trikitrok@gmail.com>
2026-08-23 13:31:20 +0200

    add new kata

---
+++ 37-parkqueen-4/src/ParkingFee.ts
@@ -0,0 +1,12 @@
+import {InvalidParkingFee} from "./InvalidParkingFee";
+
+export class ParkingFee {
+    readonly amount: number;
+
+    constructor(amount: number) {
+        if (amount < 0) {
+            throw new InvalidParkingFee('Fees cannot be negative');
+        }
+        this.amount = amount;
+    }
+}
---
+++ 37-parkqueen-4/tests/TestHelpers.ts
@@ -0,0 +1,17 @@
+import {TimeInsideParking} from "../src/TimeInsideParking";
+import {ParkingFee} from "../src/ParkingFee";
+
+export function hoursInsideParking(hours: number): TimeInsideParking {
+    const entryTime = new Date();
+    const exitTime = addHoursTo(entryTime, hours);
+    return new TimeInsideParking(entryTime, exitTime);
+}
+
+export function addHoursTo(enterTime: Date, hours: number): Date {
+    const millisecondsPerHour = 1000 * 60 * 60;
+    return new Date(enterTime.getTime() + millisecondsPerHour * hours);
+}
+
+export function parkingFeeOf(amount: number): ParkingFee {
+    return new ParkingFee(amount);
+}
